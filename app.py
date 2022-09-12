4d9d97ceda32c6f06656b7da8cd017e3ee9e98a5
Jp <jayaprakashnis619@gmail.com>
2022-09-12 16:01:52 +0530

    "added ask & memory method"

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import date
+import openai
+import os
+import requests
+from dotenv import load_dotenv
+from random import choice
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+completion = openai.Completion()
+
+key1 = 'sk-OT1DX8BR9uK1lBJtRJWsT3'
+key2 = 'BlbkFJc606VQZz5lOuiIshP06n'
+openai.api_key = f"{key1}{key2}"
 app = Flask(__name__)
 
 
